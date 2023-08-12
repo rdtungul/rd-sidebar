539c9bd980940ae4d547bfcc4870dde4b86ea4f9
rdtungul <rosephdarl@gmail.com>
2023-08-12 11:53:25 +0800

    RD Sidebar, first initial commit

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { AppContext, useGlobalContext } from './context'
+
+const Home = () => {
+  // show/hide modal and sidebar implementation
+  const { openSidebar, openModal } = useGlobalContext()
+  // console.log(data)
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  )
+}
+
+export default Home
